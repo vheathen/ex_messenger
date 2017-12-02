7134c0d7dd8bc61d65c3590c08b4ecf3297f0cb9
heathen <v.heathen@gmail.com>
2017-12-03 02:55:58 +0500

    Boilerplace code removed from ExSmsBliss \ Test

--- lib/ex_smsbliss.ex
+++ lib/ex_smsbliss.ex
@@ -2,17 +2,4 @@ defmodule ExSmsBliss do
   @moduledoc """
   Documentation for ExSmsbliss.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExSmsBliss.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/ex_smsbliss_test.exs
+++ test/ex_smsbliss_test.exs
@@ -2,7 +2,4 @@ defmodule ExSmsBlissTest do
   use ExUnit.Case
   doctest ExSmsBliss
 
-  test "greets the world" do
-    assert ExSmsBliss.hello() == :world
-  end
 end
