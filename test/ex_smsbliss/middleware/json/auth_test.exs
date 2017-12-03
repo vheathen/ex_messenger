7e441a0c86fae60c749f0ef3a0575cf29ac33abe
heathen <v.heathen@gmail.com>
2017-12-03 16:38:53 +0500

    Middleware.Json.AuthTest become more detailed and precise

--- test/ex_smsbliss/middleware/json/auth_test.exs
+++ test/ex_smsbliss/middleware/json/auth_test.exs
@@ -5,10 +5,11 @@ defmodule ExSmsBliss.Middleware.Json.AuthTest do
     use Tesla
 
     plug ExSmsBliss.Middleware.Json.Auth
+    plug Tesla.Middleware.JSON
 
     adapter fn env ->
       case env.url do
-        "/json-auth" -> env
+        "/json-auth" -> %Tesla.Env{body: ~s({"original_body":#{env.body}}), headers: env.headers}
         # "/json-reply" -> 
       end
     end
@@ -23,22 +24,29 @@ defmodule ExSmsBliss.Middleware.Json.AuthTest do
     login = auth |> Keyword.get(:login)
     password = auth |> Keyword.get(:password)
     
-    %{body: body} = JsonAuthClient.post("/json-auth", %{some_key: "some_value"})
-    assert Map.has_key?(body, :login)
-    assert Map.has_key?(body, :password)
-    assert login == Map.get(body, :login)
-    assert password == Map.get(body, :password)
+    %{body: %{"original_body" => o_body}} = JsonAuthClient.post("/json-auth", %{some_key: "some_value"})
+    assert Map.has_key?(o_body, "login")
+    assert Map.has_key?(o_body, "password")
+    assert login == Map.get(o_body, "login")
+    assert password == Map.get(o_body, "password")
   end
 
   test "login\\pass must not be changed if they are already in the body" do
     login = "new_opted_login"
     password = "new_opted_password"
     
-    %{body: body} = JsonAuthClient.post("/json-auth", %{some_key: "some_value", login: login, password: password})
-    assert Map.has_key?(body, :login)
-    assert Map.has_key?(body, :password)
-    assert login == Map.get(body, :login)
-    assert password == Map.get(body, :password)
+    %{body: %{"original_body" => o_body}} = JsonAuthClient.post("/json-auth", %{some_key: "some_value", login: login, password: password})
+    assert Map.has_key?(o_body, "login")
+    assert Map.has_key?(o_body, "password")
+    assert login == Map.get(o_body, "login")
+    assert password == Map.get(o_body, "password")
+  end
+
+  test "login\\pass must not exist in the answer" do    
+    %{body: r_body} = JsonAuthClient.post("/json-auth", %{some_key: "some_value"})
+
+    refute Map.has_key?(r_body, "login")
+    refute Map.has_key?(r_body, "password")
   end
 
   test "add_auth/2: login\\pass must be set from opts if they are there" do
