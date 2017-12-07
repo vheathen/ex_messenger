7cbee9f37b420d38ec3c21cea33a767d2bef2c11
heathen <v.heathen@gmail.com>
2017-12-08 01:25:39 +0500

    Main work scheme

--- lib/ex_smsbliss.ex
+++ lib/ex_smsbliss.ex
@@ -1,5 +1,14 @@
 defmodule ExSmsBliss do
   @moduledoc """
   Documentation for ExSmsbliss.
+
+  Write a message to the storage with status :queued
+  Every N time units start process to push all :queued messages to the service
+  Get :queued messages and change their status to :working (in the storage)
+  Put similar messages to the batches up to 200 messages
+  Send each batch to the server and after receive status change messages' status to the received ones
+  and update their smscIds in storage
+  Every N time units get :accepted messages and send a 'status' request
+  After status changed put status to the storage
   """
 end
