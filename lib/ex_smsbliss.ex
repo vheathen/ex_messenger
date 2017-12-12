fb585f0e45ed8b05ac949e32702ead8ac92ac93c
heathen <v.heathen@gmail.com>
2017-12-12 19:21:46 +0500

    General algo review

--- lib/ex_smsbliss.ex
+++ lib/ex_smsbliss.ex
@@ -3,12 +3,16 @@ defmodule ExSmsBliss do
   Documentation for ExSmsbliss.
 
   Write a message to the storage with status :queued
-  Every N time units start process to push all :queued messages to the service
+  Every X time units start process to push all :queued messages to the service
   Get :queued messages and change their status to :working (in the storage)
   Put similar messages to the batches up to 200 messages
   Send each batch to the server and after receive status change messages' status to the received ones
   and update their smscIds in storage
-  Every N time units get :accepted messages and send a 'status' request
+  Every Y time units get :accepted messages and send a 'status' request
   After status changed put status to the storage
+
+  Every Z time units get :working queue and set it as :unknown
+
+  
   """
 end
